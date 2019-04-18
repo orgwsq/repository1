3375c8c410d4c9c590fab85350f01005ea27d8e0
wangshaoqiang123 <2382710482@qq.com>
2019-04-18 12:24:38 +0800

    第一次提交项目

---
+++ src/main/java/com/aaa/test/hello.java
@@ -0,0 +1,13 @@
+package com.aaa.test;
+
+/**
+ * fileName:hello
+ * description:
+ * author:王少强
+ * createTime:2019/4/18 12:20
+ */
+public class hello {
+    public static void main(String[] args) {
+        System.out.println("1111100");
+    }
+}
