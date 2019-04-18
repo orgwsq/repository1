1972584feec7e816b87eb98a01b22da23702d059
wangshaoqiang123 <2382710482@qq.com>
2019-04-18 13:09:40 +0800

    第一次提交项目,,,

--- src/main/java/com/aaa/test/hello.java
+++ src/main/java/com/aaa/test/hello.java
@@ -8,6 +8,6 @@ package com.aaa.test;
  */
 public class hello {
     public static void main(String[] args) {
-        System.out.println("1111100");
+        System.out.println("1111100dtt");
     }
 }
