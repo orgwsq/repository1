87cf43745d41e11473044802e26ee9c0ca287937
wangshaoqiang123 <2382710482@qq.com>
2019-04-19 09:21:03 +0800

    修改后3提交项目,,,

--- src/main/java/com/aaa/test/hello.java
+++ src/main/java/com/aaa/test/hello.java
@@ -7,7 +7,9 @@ package com.aaa.test;
  * createTime:2019/4/18 12:20
  */
 public class hello {
-    public static void main(String[] args) {
-        System.out.println("11117jvgkkdnjdsv77100dtt");
+    public static void main(String[] args)
+    {
+        System.out.println("1314");
+        System.out.println("521");
     }
 }
